856420d835596efabcb6e56f001bca9fe98d3975
MoksS <MoksShuv@yandex.ru>
2020-09-05 12:48:31 +0300

    CONTEXT remove TS

--- testTopLVLAwait.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const response = await axios('https://quote-garden.herokuapp.com/api/v2/quotes/random');
-console.log(response.data);
-
-
-// do not forget to indicate "type": "module", in package.json
